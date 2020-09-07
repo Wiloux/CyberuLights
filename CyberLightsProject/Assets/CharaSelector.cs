aad550d4d7b7e31b4c7c5e7c6830367f8b478d3a
Wiloux <bittersweetsummerdreams@gmail.com>
2020-09-07 16:01:51 +0100

    xd

---
+++ CyberLightsProject/Assets/CharaSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class CharaSelector : MonoBehaviour
+{
+
+    public Vector2 x1y2;
+    public void OnMove(CallbackContext context)
+    {
+        x1y2 = context.ReadValue<Vector2>();
+        
+    }// Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<CharaSelector>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
